6868a0d9952c96ef916ab2977f437efcdeb6fcd3
Carlos Abraao <carloschavesnd@hotmail.com>
2022-03-07 09:43:16 -0300

    criei o projeto blogAdm

---
+++ src/main/java/com/carlosdev/blogStruct/StructBlogdev/StructBlogdevApplication.java
@@ -0,0 +1,13 @@
+package com.carlosdev.blogStruct.StructBlogdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructBlogdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructBlogdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosdev/blogStruct/StructBlogdev/StructBlogdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosdev.blogStruct.StructBlogdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructBlogdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
