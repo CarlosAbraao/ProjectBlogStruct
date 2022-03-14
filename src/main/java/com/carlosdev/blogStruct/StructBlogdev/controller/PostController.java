d07b6bebf7ac59dac13dc1e70f1ef390a6beda52
Carlos Abraao <carloschavesnd@hotmail.com>
2022-03-14 12:12:22 -0300

    criada pagina de validação do formulário e add do summernote

--- src/main/java/com/carlosdev/blogStruct/StructBlogdev/controller/PostController.java
+++ src/main/java/com/carlosdev/blogStruct/StructBlogdev/controller/PostController.java
@@ -5,13 +5,17 @@ import com.carlosdev.blogStruct.StructBlogdev.InterfacePostService;
 import com.carlosdev.blogStruct.StructBlogdev.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -39,4 +43,31 @@ public class PostController {
         return mv;
 
     }
+
+
+    @RequestMapping(value = "/v1newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return "formulario";
+    }
+
+
+    @RequestMapping(value = "/v1newpost", method = RequestMethod.POST)
+    public String SavePostForm(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        // VALIDAÇÃO DO POST, NO CASO UMA REGRA DE NEGOCIO QUE PODE IR PARA O SERVICE
+        if (result.hasErrors()){
+
+            attributes.addFlashAttribute("message","Verifique se os campos obrigatórios foram preenchidos.");
+            return "redirect:/v1newpost";
+        }
+
+        //  INSERINDO DATA NO POST JA QUE A MESMA NÃO ESTARÁ NO FORMULARIO
+        post.setPostDate(LocalDate.now());
+
+        // SALVANDO O POST
+        postService.save(post);
+
+        //RETOTNANDO A PAGINA COM OS POSTS
+        return "redirect:/v1posts";
+    }
+
 }
